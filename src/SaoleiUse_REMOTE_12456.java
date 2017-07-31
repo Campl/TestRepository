e4098a7f5eab10b2b889ab0271787657bf62af5c
Lenovo-PC\campl <645003536@qq.com>
2017-07-31 15:03:51 +0800

    0731 15;03

---
+++ src/SaoleiUse_REMOTE_12456.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+//import java.awt.*;
+//import java.awt.event.*;
+//changes_1
+
+public class SaoleiUse {
+	public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {  
+            @Override  
+            public void run() {  
+            	SaoleiMain s=new SaoleiMain();
+            	s.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            	s.setVisible(true);
+            }  
+        });
+	}
+}
